5b5090f8fb2c7cda30a9e183a03b427c28b19f2a
Tommy <Tommy>
2015-09-26 16:14:23 -0700

    added a debug object to indicate when pencil is in margin

--- Assets/MouseListener.cs
+++ Assets/MouseListener.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class MouseListener : MonoBehaviour {
 
     GameObject paper = null;
+    GameObject debug = null;
 
 	// Use this for initialization
 	void Start () {
         paper = GameObject.Find("Paper");
-        //transform.rotation = paper.transform.rotation.;
-
+        debug = GameObject.Find("Debug");
 	}
 
-	// Update is called once per frame
-	void Update () {
-        float x = Input.mousePosition.x / Screen.width - .5f;
-        float y = Input.mousePosition.y / Screen.height - .5f;
-        x *= paper.transform.localScale.x;
-        y *= paper.transform.localScale.z;
+	void FixedUpdate () {
+        float mx = Input.mousePosition.x / Screen.width;
+        float my = Input.mousePosition.y / Screen.height;
+        float x = (mx-.5f) * paper.transform.localScale.x;
+        float y = (my-.5f) * paper.transform.localScale.z;
         transform.position = paper.transform.position + paper.transform.rotation * new Vector3(x, 0, y);
-	}
+
+        if (mx < 0.3)
+            debug.transform.position = new Vector3(1.65f, 3, -8.35f);
+        else
+            debug.transform.position = new Vector3(1.65f, 1.5f, -8.35f);
+    }
 }
