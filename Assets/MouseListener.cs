a974900b225c1a757e0a8cbc804bfa9baa6e594a
Tommy <Tommy>
2015-09-26 14:29:47 -0700

    added MouseListener.cs

---
+++ Assets/MouseListener.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Scale(Input.mousePosition,
+		                                   new Vector3(1.0f/Screen.width, 1.0f/Screen.height, 1));
+	}
+}
