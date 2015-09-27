1b8b7809f3bce81fdac8ad60d966c93bedfc1cf2
Derek Donahue <propolandante@gmail.com>
2015-09-26 18:51:27 -0700

    Basic writing functionality implemented. Next step is interpolating lines between the points

--- Assets/MouseListener.cs
+++ Assets/MouseListener.cs
@@ -6,6 +6,9 @@ public class MouseListener : MonoBehaviour {
     GameObject paper = null;
     GameObject debug = null;
 
+    public float mx;
+    public float my;
+
 	// Use this for initialization
 	void Start () {
         paper = GameObject.Find("Paper");
@@ -13,15 +16,20 @@ public class MouseListener : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-        float mx = Input.mousePosition.x / Screen.width;
-        float my = Input.mousePosition.y / Screen.height;
-        float x = (mx-.5f) * paper.transform.localScale.x;
-        float y = (my-.5f) * paper.transform.localScale.z;
-        transform.position = paper.transform.position + paper.transform.rotation * new Vector3(x, 0, y);
+        mx = (Input.mousePosition.x / Screen.width);
+        my = (Input.mousePosition.y / Screen.height);
+        float x = (mx-.5f) * paper.transform.localScale.x * -1f;
+        float y = (my-.5f) * paper.transform.localScale.z * -1f;
+        transform.position = paper.transform.position + paper.transform.rotation * new Vector3(x, 1, y);
 
         if (mx < 0.3)
             debug.transform.position = new Vector3(1.65f, 3, -8.35f);
         else
             debug.transform.position = new Vector3(1.65f, 1.5f, -8.35f);
+    }
+
+    public Vector2 getScreenCoords()
+    {
+        return new Vector2(mx, my);
     }
 }
