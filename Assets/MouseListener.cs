868cfae132e754227bc52375017aed200df4f319
Tommy <Tommy>
2015-09-26 21:59:34 -0700

    added code for animating AI handwriting

--- Assets/MouseListener.cs
+++ Assets/MouseListener.cs
@@ -12,13 +12,92 @@ public class MouseListener : MonoBehaviour {
         debug = GameObject.Find("Debug");
 	}
 
-	void FixedUpdate () {
+    class Motion
+    {
+        public enum Type {MOVED, DRAGGED, DOT}
+        public Vector2 pos;
+        public float time;
+        public Type type;
+        Motion(string s, float t, float x, float y)
+        {
+            pos = new Vector2(x, y);
+            time = t;
+            if (s == "moved")
+                type = Type.MOVED;
+            else if (s == "dragged")
+                type = Type.DRAGGED;
+            else if (s == "dot")
+                type = Type.DOT;
+        }
+    }
+
+    void moveHand(Vector2 v)
+    {
+
+    }
+    AIState state;
+
+    class Idling : AIState
+    {
+        public static Idling instance = new Idling();
+        public void update() {}
+        private Idling() {}
+    }
+    class Animating : AIState
+    {
+        int frame;
+        float start_time;
+        Motion[] animation;
+        MouseListener parent;
+        public Animating(Motion[] a, MouseListener p)
+        {
+            frame = 0;
+            start_time = Time.time;
+            animation = a;
+            parent = p;
+        }
+
+        public void update()
+        {
+            float time = Time.time;
+            while (time - start_time < animation[frame + 1].time)
+            {
+                ++frame;
+
+                switch(animation[frame].type)
+                {
+                    case Motion.Type.DOT:
+                        writeDot(animation[frame].pos);
+                        break;
+                    case Motion.Type.DRAGGED:
+                        writeLine(animation[frame-1].pos, animation[frame].pos);
+                        break;
+                }
+            }
+
+            parent.moveHand(animation[frame].pos);
+        }
+    }
+    interface AIState
+    {
+        void update();
+    }
+    void startAnimation(Motion[] animation)
+    {
+        state = new Animating(animation, this);
+    }
+
+    void Update()
+    {
+        state.update();
+    }
+
+    void FixedUpdate() {
         float mx = Input.mousePosition.x / Screen.width;
         float my = Input.mousePosition.y / Screen.height;
-        float x = (mx-.5f) * paper.transform.localScale.x;
-        float y = (my-.5f) * paper.transform.localScale.z;
+        float x = (mx - .5f) * paper.transform.localScale.x;
+        float y = (my - .5f) * paper.transform.localScale.z;
         transform.position = paper.transform.position + paper.transform.rotation * new Vector3(x, 0, y);
-
         if (mx < 0.3)
             debug.transform.position = new Vector3(1.65f, 3, -8.35f);
         else
